b021a7a49c7259ced1d1e859680e9c4e45dfe180
saksham sharma <70438649+S-SQuare-Hub@users.noreply.github.com>
2022-05-28 19:59:56 -0400

    Updated config

--- crs-web-app/src/Firebaseconfig.js
+++ crs-web-app/src/Firebaseconfig.js
@@ -14,12 +14,12 @@ import 'firebase/compat/database';
 };*/
 
 const config = {
-    apiKey: "AIzaSyAdMTKBXxEt4RlB-n7ltgzh2kS-bZgs_H8",
-    authDomain: "its-crs.firebaseapp.com",
-    projectId: "its-crs",
-    storageBucket: "its-crs.appspot.com",
-    messagingSenderId: "908728961180",
-    appId: "1:908728961180:web:153c62505ca6b99c6c8559"
+    apiKey: "AIzaSyAgGS7HCJENK9kkuPHDKPNlSfj2OoHHde4",
+    authDomain: "campus-recruitment-system-cr.firebaseapp.com",
+    databaseURL: "https://campus-recruitment-system-cr.firebaseio.com",
+    projectId: "campus-recruitment-system-cr",
+    storageBucket: "campus-recruitment-system-cr.appspot.com",
+    messagingSenderId: "864262446127"
 };
 
 const firebaseconfig = firebase.initializeApp(config);
